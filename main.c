7d4804aaf65254fce759c3f70c7fffce74bc00e2
CallmeElodie <lfam5718@gmail.com>
2025-07-31 08:50:16 -0500

    velocity

--- main.c
+++ main.c
@@ -1,82 +1,84 @@
 #include "raylib.h"
 
 int main(){
-
     const int screenWidth = 800;
     const int screenHeight = 450;
-
     InitWindow(screenWidth, screenHeight, "Repetitime");
-
-    // Colors
-
+    
+    // colors
     Color colorBackground = GetColor(0xcbb99dFF);
     Color colorMiddleground1 = GetColor(0x8b6341FF);
     Color colorForeground1 = GetColor(0x4e2d18FF);
-
     Color colorMiddleground2 = GetColor(0x112921FF);
     Color colorForeground2 = GetColor(0x4d6443FF);
-
-
-    Rectangle player = { 100, 100, 50, 50 };
+    
+    typedef struct MovableRectangle {
+        Rectangle rect;
+        Vector2 velocity;
+    } MovableRectangle;
+    
+    MovableRectangle player = { { 100, 100, 50, 50 }, { 0, 0 } };
     Rectangle ground = { 0, 400, 800, 50 };
-
-    bool isGrounded;
+    
+    const float JUMP_VELOCITY = -11.0f;
+    const float GRAVITY = 0.5f;
+    
+    bool isGrounded = false;
     bool showText = true;
-    const char* message = "Welcome to Repeti-time\n\np,Press any key to play";
-
+    const char* message = "Welcome to Repeti-time\n\nPress any key to play\nA/D to move, SPACE to jump";
+    
     SetTargetFPS(60);
-
+    
     while (!WindowShouldClose()){
-
-
         if (GetKeyPressed() != 0){
             showText = false;
         }
-
+        
         if (!showText){
 
-            player.y += 4;
-
-            DrawRectangle(player.x, player.y, player.width, player.height, colorForeground2);
-            DrawRectangle(ground.x, ground.y, ground.width, ground.height, colorMiddleground1);
-
             if (IsKeyDown(KEY_A)){
-                player.x -= 4;
+                player.rect.x -= 4;
             }
             if (IsKeyDown(KEY_D)){
-                player.x += 4;
+                player.rect.x += 4;
             }
-
-            if (CheckCollisionRecs(player, ground)){
-                
-                Rectangle overlap = GetCollisionRec(player, ground);
-                
-                player.y -= overlap.height; 
             
-                isGrounded = true; 
+            if (IsKeyPressed(KEY_SPACE) && isGrounded){
+                player.velocity.y = JUMP_VELOCITY;
             }
-            else
-            {
+            
+            // gravity
+            player.velocity.y += GRAVITY;
+            
+            // Apply velocity to position
+            player.rect.y += player.velocity.y;
+            
+            // collision
+            if (CheckCollisionRecs(player.rect, ground)){
+                Rectangle overlap = GetCollisionRec(player.rect, ground);
+                player.rect.y -= overlap.height;
+                player.velocity.y = 0;
+                isGrounded = true;
+            }
+            else {
                 isGrounded = false;
             }
-
-
-
         }
-
+        
         BeginDrawing();
-
         ClearBackground(colorBackground);
-
+        
         if (showText){
-            DrawText(message, (screenWidth/2) - (MeasureText(message, 20)/2) , (screenHeight/2) - (20/2), 20, colorForeground1);
+            DrawText(message, (screenWidth/2) - (MeasureText(message, 20)/2), (screenHeight/2) - (20/2), 20, colorForeground1);
         }
-
+        else {
+            DrawRectangle(player.rect.x, player.rect.y, player.rect.width, player.rect.height, colorForeground2);
+            DrawRectangle(ground.x, ground.y, ground.width, ground.height, colorMiddleground1);
+        }
+        
         EndDrawing();
     }
-
-
+    
     CloseWindow();
-
     return 0;
 }
