7048c0e4ad7e462f23cbee8f4399020f57e9012b
CallmeElodie <lfam5718@gmail.com>
2025-07-31 09:59:24 -0500

    responsive jumping

--- main.c
+++ main.c
@@ -18,12 +18,19 @@ int main(){
     } MovableRectangle;
     
     MovableRectangle player = { { 100, 100, 50, 50 }, { 0, 0 } };
-    Rectangle ground = { 0, 400, 800, 50 };
+    Rectangle ground = { 0, 400, 4000, 400 };
+
+    Camera2D camera = { 0 };
+    camera.target = (Vector2){ player.rect.x, player.rect.y }; // Initial target
+    camera.offset = (Vector2){ GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f }; // Center of screen
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
     
     const float JUMP_VELOCITY = -11.0f;
     const float GRAVITY = 0.5f;
     
     bool isGrounded = false;
+    bool jumpRequested = false;
     bool showText = true;
     const char* message = "Welcome to Repeti-time\n\nPress any key to play\nA/D to move, SPACE to jump";
     
@@ -43,14 +50,20 @@ int main(){
                 player.rect.x += 4;
             }
             
-            if (IsKeyPressed(KEY_SPACE) && isGrounded){
-                player.velocity.y = JUMP_VELOCITY;
+            if (IsKeyPressed(KEY_SPACE)){
+                jumpRequested = true;
             }
+            if (jumpRequested && isGrounded){
+                player.velocity.y = JUMP_VELOCITY;
+                jumpRequested = false;
+            }
+
+            camera.target = (Vector2){ player.rect.x, player.rect.y };
             
             // gravity
             player.velocity.y += GRAVITY;
             
-            // Apply velocity to position
+            // apply velocity to position
             player.rect.y += player.velocity.y;
             
             // collision
@@ -66,16 +79,18 @@ int main(){
         }
         
         BeginDrawing();
+        BeginMode2D(camera);
         ClearBackground(colorBackground);
         
         if (showText){
-            DrawText(message, (screenWidth/2) - (MeasureText(message, 20)/2), (screenHeight/2) - (20/2), 20, colorForeground1);
+            DrawText(message, (screenWidth/20) - (MeasureText(message, 20)/6), (screenHeight/6) - (20/6), 20, colorForeground1);
         }
         else {
             DrawRectangle(player.rect.x, player.rect.y, player.rect.width, player.rect.height, colorForeground2);
             DrawRectangle(ground.x, ground.y, ground.width, ground.height, colorMiddleground1);
         }
         
+        EndMode2D();
         EndDrawing();
     }
     
